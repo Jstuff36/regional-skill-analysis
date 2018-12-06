eca3d562c9b46e2cce0abf8d99478e0bea3f99a5
Jstuff36 <mizzou_fan10@hotmail.com>
2018-12-06 07:55:19 -0500

    Add state and functionality to login navbar

--- src/Frontend/Components/NavBar/NavBar.tsx
+++ src/Frontend/Components/NavBar/NavBar.tsx
@@ -1,17 +1,39 @@
 import * as React from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, InputOnChangeData } from 'semantic-ui-react';
 import '../../Styles/NavBar.css';
 
-export class NavBar extends React.Component {
+interface State {
+    email: string;
+    password: string;
+}
+
+export class NavBar extends React.Component<{}, State> {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    onEmailChange = (_: React.ChangeEvent<HTMLInputElement>, { value }: InputOnChangeData) => this.setState({ email: value });
+    onPasswordChange = (_: React.ChangeEvent<HTMLInputElement>, { value }: InputOnChangeData) => this.setState({ password: value });
+
     render() {
+
+        const {email, password} = this.state;
+
         return (
             <div className="positioningContainer">
                 <div className="navBarContainer">
                     <div className="navBarName">Regional Skill Analysis</div>
                     <Form className="loginOptions">
                         <Form.Group>
-                            <Form.Input label={"Email"} />
-                            <Form.Input label={"Password"}/>                    
+                            <Form.Input value={email} label={"Email"} onChange={this.onEmailChange} />
+                            <Form.Input 
+                                value={password} 
+                                label={"Password"} 
+                                type={"password"} 
+                                onChange={this.onPasswordChange}
+                            />                    
                             <Form.Button content={"Log In"}/>
                         </Form.Group>
                     </Form>
