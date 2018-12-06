2101b03d70310bec5ad2d580eb13af28c3989b4c
Jstuff36 <mizzou_fan10@hotmail.com>
2018-12-06 07:30:00 -0500

    Finish styling login navbar and start login page

--- src/Frontend/Components/LoginPage.tsx
+++ src/Frontend/Components/LoginPage.tsx
@@ -1,10 +1,45 @@
 import * as React from 'react';
+import '../Styles/loginContainer.css';
+import { Form, Icon } from 'semantic-ui-react';
 
-export class LoginPage extends React.Component {
+interface State {
+    showPassword: boolean;
+}
+
+export class LoginPage extends React.Component<{}, State> {
+
+    state = {
+        showPassword: false
+    }
+
     render() {
+        const { showPassword } = this.state;
         return (
-            <div>
-                HI
+            <div className="loginContainer">
+                <div className="leftSide">
+                    <div>Large text</div>
+                    <div>some more text explianing whats going on</div>
+                    <div>some point</div>
+                    <div>another point yeyyyyyyyyy</div>
+                </div>
+                <div className="rightSide">
+                    <div>Sign Up</div>
+                    <Form>
+                        <Form.Input label={"Company Name"}/>
+                        <Form.Input label={"Email"} required/>
+                        <Form.Input 
+                            required
+                            label={"Password"} 
+                            type={showPassword ? "" : "password"} 
+                            icon={showPassword ? 
+                                <Icon name={"eye slash"} onClick={() => this.setState({showPassword: false})}/> 
+                                : 
+                                <Icon name={"eye"} onClick={() => this.setState({showPassword: true})}/>
+                            } 
+                        />
+                        <Form.Button content={"Sign Up"} />
+                    </Form>
+                </div>
             </div>
         )
     }
--- src/Frontend/Components/NavBar/NavBar.tsx
+++ src/Frontend/Components/NavBar/NavBar.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import { Input, Button } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import '../../Styles/NavBar.css';
 
 export class NavBar extends React.Component {
     render() {
         return (
-            <div className="navBarContainer">
-                <span>Regional Skill Analysis</span>
-                <Input label={"Email"}/>
-                <Input label={"Password"}/>
-                <Button content={"Log In"}/>
+            <div className="positioningContainer">
+                <div className="navBarContainer">
+                    <div className="navBarName">Regional Skill Analysis</div>
+                    <Form className="loginOptions">
+                        <Form.Group>
+                            <Form.Input label={"Email"} />
+                            <Form.Input label={"Password"}/>                    
+                            <Form.Button content={"Log In"}/>
+                        </Form.Group>
+                    </Form>
+                </div>
             </div>
         )
     }
