03f46de894e78fdb74ac645935c985e05580d220
Jstuff36 <mizzou_fan10@hotmail.com>
2018-12-05 21:57:20 -0500

    Set up ts linter and begin workin on navbar

--- src/Frontend/Components/NavBar/NavBar.tsx
+++ src/Frontend/Components/NavBar/NavBar.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { Input, Button } from 'semantic-ui-react';
+import '../../Styles/NavBar.css';
 
 export class NavBar extends React.Component {
     render() {
         return (
-            <div>
-                NavBar
+            <div className="navBarContainer">
+                <span>Regional Skill Analysis</span>
+                <Input label={"Email"}/>
+                <Input label={"Password"}/>
+                <Button content={"Log In"}/>
             </div>
         )
     }
