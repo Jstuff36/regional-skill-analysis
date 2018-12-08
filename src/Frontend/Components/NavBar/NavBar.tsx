b93336cd4e1b8ab728bfa329dfb19c0c32da67ea
Jstuff36 <mizzou_fan10@hotmail.com>
2018-12-07 22:44:01 -0500

    Start login navbar

--- src/Frontend/Components/NavBar/NavBar.tsx
+++ src/Frontend/Components/NavBar/NavBar.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import { Form, InputOnChangeData } from 'semantic-ui-react';
 import '../../Styles/NavBar.css';
+import { StoreState } from 'src/Frontend/Reducers/rootReducer';
+import { connect } from 'react-redux';
 
 interface State {
     email: string;
     password: string;
 }
 
-export class NavBar extends React.Component<{}, State> {
+interface StateProps {
+    loggedIn: boolean;
+}
+
+class NavBarComponent extends React.Component<StateProps, State> {
 
     state = {
         email: '',
-        password: '',
+        password: ''
     }
 
     onEmailChange = (_: React.ChangeEvent<HTMLInputElement>, { value }: InputOnChangeData) => this.setState({ email: value });
@@ -19,26 +25,44 @@ export class NavBar extends React.Component<{}, State> {
 
     render() {
 
+        const {loggedIn} = this.props;
         const {email, password} = this.state;
 
-        return (
-            <div className="positioningContainer">
-                <div className="navBarContainer">
-                    <div className="navBarName">Regional Skill Analysis</div>
-                    <Form className="loginOptions">
-                        <Form.Group>
-                            <Form.Input value={email} label={"Email"} onChange={this.onEmailChange} />
-                            <Form.Input 
-                                value={password} 
-                                label={"Password"} 
-                                type={"password"} 
-                                onChange={this.onPasswordChange}
-                            />                    
-                            <Form.Button content={"Log In"}/>
-                        </Form.Group>
-                    </Form>
+        if (loggedIn) {
+            return (
+                <div className="positioningContainer">
+                    <div className="navBarContainer">
+                        <div className="navBarName">Regional Skill Analysis</div>
+                        <div>Search by </div>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="positioningContainer">
+                    <div className="navBarContainer">
+                        <div className="navBarName">Regional Skill Analysis</div>
+                        <Form className="loginOptions">
+                            <Form.Group>
+                                <Form.Input value={email} label={"Email"} onChange={this.onEmailChange} />
+                                <Form.Input
+                                    value={password}
+                                    label={"Password"}
+                                    type={"password"}
+                                    onChange={this.onPasswordChange}
+                                />
+                                <Form.Button content={"Log In"} />
+                            </Form.Group>
+                        </Form>
+                    </div>
+                </div>
+            )
+        }
     }
 }
+
+const mapStateToProps = (store: StoreState): StateProps => {
+    return { loggedIn: true }
+}
+
+export const NavBar = connect(mapStateToProps)(NavBarComponent);
